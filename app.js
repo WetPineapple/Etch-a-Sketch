23b387a3eec46d451fd6d85fabfdee406dfd9e0a
Wetpineapple <leonard.towne@outlook.com>
2022-04-05 21:14:57 -0500

    updates

---
+++ app.js
@@ -0,0 +1,10 @@
+const gridLayout = document.querySelector('#layout');
+
+
+for (let i = 0; i <= 16; i ++) {
+
+    const newSquare= document.createElement('div');
+    newSquare.src = "test";
+    gridLayout.appendChild(newSquare);
+    
+}
